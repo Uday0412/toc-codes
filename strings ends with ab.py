0e7355b8bb7f4e1778ce8d92b9191bb3f5160a51
Uday0412 <145842199+Uday0412@users.noreply.github.com>
2024-07-07 20:10:13 +0530

    date 7-7-2024
    toc code in python

---
+++ strings ends with ab.py
@@ -0,0 +1,25 @@
+class DFA:
+    def __init__(self):
+        self.states = {'q0', 'q1'}
+        self.alphabet = {'a', 'b'}
+        self.start_state = 'q0'
+        self.accept_states = {'q1'}
+        self.transitions = {
+            ('q0', 'b'): 'q0',
+            ('q0', 'a'): 'q0',
+            ('q0', 'a'): 'q1',
+        }
+
+    def accepts(self, string):
+        current_state = self.start_state
+        for char in string:
+            current_state = self.transitions.get((current_state, char), 'q2')
+        return current_state in self.accept_states
+
+# Example usage
+dfa = DFA()
+
+test_strings = ["a","aa","aaa","ba","baa","bbba"]
+for string in test_strings:
+    result = dfa.accepts(string)
+    print(f"String '{string}' is accepted: {result}")
---
+++ strins start with 101.py
@@ -0,0 +1,32 @@
+class DFA:
+    def __init__(self):
+        self.states = {'q0', 'q1', 'q2','q3','q4'}
+        self.alphabet = {'0', '1'}
+        self.start_state = 'q0'
+        self.accept_states = {'q1'}
+        self.accept_states = {'q2'}
+        self.accept_states = {'q3'}
+        self.transitions = {
+            ('q0', '1'): 'q1',
+            ('q0', '0'): 'q4',
+            ('q1', '0'): 'q2',
+            ('q1', '1'): 'q4',
+            ('q2', '1'): 'q3',
+            ('q2', '0'): 'q4',
+            ('q3', '0'): 'q3',
+            ('q3', '1'): 'q3',
+        }
+
+    def accepts(self, string):
+        current_state = self.start_state
+        for char in string:
+            current_state = self.transitions.get((current_state, char), 'q2')
+        return current_state in self.accept_states
+
+# Example usage
+dfa = DFA()
+
+test_strings = ["1011","1010","1011","101111"]
+for string in test_strings:
+    result = dfa.accepts(string)
+    print(f"String '{string}' is accepted: {result}")
